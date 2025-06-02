f659e652a749a7102ed834059aed43a122ebfb05
Siwaphon Panisen <siwaphon08@gmail.com>
2025-06-02 12:05:33 +0700

    add hero & stats

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,50 @@
+import Image from "next/image"
+import Pretitle from "./Pretitle"
+import Button from "./Button"
+
 const About = () => {
   return (
-    <div id="about" className="h-screen bg-red-200">About</div>
+    <div className="pt-16 xl:pt-32" id="about">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-12 xl:gap-0 xl:flex-row xl:items-center">
+          <div className="flex-1">
+            <div className="max-w-[540px]">
+              <Pretitle text="About us"/>
+              <h2>
+                Focused On Excellence In Every Project
+              </h2>
+              <p>
+                Our unweavering commitment to excellence dirves every project we 
+                undertake. From concept to completion, we meticulously craft
+                solutions that embody quality, precision and innovation.
+              </p>
+              <div className="w-max flex flex-col text-right mb-10">
+                <Image
+                 src="/assets/img/about/signature.svg"
+                 width={154}
+                 height={38}
+                 alt=""
+                 />
+                 <p>Company CEO</p>
+              </div>
+              <Button text="Contact us"/>
+            </div>
+          </div>
+          <div className="flex-1 xl:flex xl:justify-center">
+            <div className="xl:w-[444px] xl:h-[493px] relative">
+              <div className="hidden xl:flex w-[444px] h-[493px] bg-accent absolute
+              -top-4 -left-4 -z-10"></div>
+              <Image
+                src="/assets/img/about/img.jpg"
+                width={444}
+                height={493}
+                alt="" 
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,27 @@
+import Button from "./Button"
+
 const Hero = () => {
-  return <section className="h-screen bg-blue-200">hero</section>
+  return (
+    <section className="h-[70vh] bg-[url('/assets/img/hero/bg.jpg')] bg-no-repeat bg-cover bg-center relative">
+      <div className="absolute inset-0 bg-gradient-to-l from-black/0 via-black/50 to-black/70 z-10">
+        <div className="container mx-auto h-full flex items-center">
+          <div className="z-20 text-white text-center xl:text-left mx-auto xl:mx-0 flex flex-col items-center xl:items-start max-w-[608px]">
+            <h1 className="h1  text-white mb-4">
+              <span className="text-accent">Building</span> robust lasting
+              solutions
+            </h1>
+            <p className="mb-9">From concept to completion, we ensure every details is optimized for 
+              strength and endurance, creating solutins that inspire confidence and stand 
+              firm for years.
+            </p>
+            <div>
+              <Button text="See our work"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
